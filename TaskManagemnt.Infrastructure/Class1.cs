65f6999730768f9c14609170d31931c3742131b9
mina_pst <minapakdast@gmail.com>
2026-08-30 09:12:55 +0300

    feat: add EF Core Task DbContext

--- TaskManagemnt.Infrastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TaskManagemnt.Infrastructure
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ TaskManagemnt.Infrastructure/TaskDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagemnt.Entities;
+
+namespace TaskManagement.Infrastructure.Data;
+
+public class TaskDbContext : DbContext
+{
+    public TaskDbContext(DbContextOptions<TaskDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>(entity =>
+        {
+            entity.ToTable("Tasks");
+
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.Title).IsRequired().HasMaxLength(200);
+
+            entity.Property(x => x.Description).HasMaxLength(1000);
+        });
+    }
+}
