a8612dbd952aeea6b0aa3756c5922751843effab
mina_pst <minapakdast@gmail.com>
2026-08-30 14:27:29 +0300

    feat: add swwagger configuration

--- TaskManagemnt.Api/Program.cs
+++ TaskManagemnt.Api/Program.cs
@@ -19,9 +19,13 @@ builder.Services.AddScoped<TaskService>();
 builder.Services.AddScoped<IValidator<TaskItem>,TaskValidator>();
 
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
