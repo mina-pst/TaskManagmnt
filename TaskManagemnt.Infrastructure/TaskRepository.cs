3f7e6f6818ee17bc91c5279ebeae4810941c8999
mina_pst <minapakdast@gmail.com>
2026-08-30 09:31:53 +0300

    feat: add task repository

---
+++ TaskManagemnt.Infrastructure/TaskRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Infrastructure.Data;
+using TaskManagemnt.Entities;
+using TaskManagemnt.UseCases.Interfaces;
+namespace TaskManagement.Infrastructure.Repositories;
+public class TaskRepository : ITaskRepository
+{
+    private readonly TaskDbContext _context;
+    public TaskRepository(TaskDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<TaskItem?> GetByIdAsync(int id)
+    {
+        return await _context.Tasks.FirstOrDefaultAsync(x => x.Id == id);
+    }
+    public async Task AddAsync(TaskItem taskItem)
+    {
+        await _context.Tasks.AddAsync(taskItem);
+    }
+    public Task UpdateAsync(TaskItem taskItem)
+    {
+        _context.Tasks.Update(taskItem);
+
+        return Task.CompletedTask;
+    }
+    public Task DeleteAsync(TaskItem taskItem)
+    {
+        _context.Tasks.Remove(taskItem);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ TaskManagemnt.UseCases/Interfaces/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using TaskManagemnt.Entities;
+
+namespace TaskManagemnt.UseCases.Interfaces
+{
+   public interface ITaskRepository
+    {
+        Task<TaskItem?> GetByIdAsync(int Id);
+        Task AddAsync(TaskItem item);
+        Task UpdateAsync(TaskItem item);
+        Task DeleteAsync(TaskItem item);
+    }
+}
